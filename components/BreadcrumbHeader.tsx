68e02b8e9c880ea0d0ce0aa06fdf0f3865610191
patryk <paniczgg@gmail.com>
2025-02-27 03:46:48 +0100

    before auth added mobile and desktop siebar plus not-found page

---
+++ components/BreadcrumbHeader.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+} from "./ui/breadcrumb";
+import { Fragment } from "react";
+import { MobileSidebar } from "./Siebar";
+
+export const BreadcrumbHeader = () => {
+  const pathname = usePathname();
+  const paths = pathname === "/" ? [""] : pathname.split("/");
+  return (
+    <div className="flex items-center flex-start">
+      <MobileSidebar />
+      <Breadcrumb>
+        <BreadcrumbList>
+          {paths.map((path, index) => (
+            <Fragment key={index}>
+              <BreadcrumbItem>
+                <BreadcrumbLink className="capitalize" href={`/${path}`}>
+                  {path === "" ? "home" : path}
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </Fragment>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+};
